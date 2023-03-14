f02bb46d2f8f2ed9e704845c6fe51de684ca68d3
costinp02 <costinpin02@gmail.com>
2023-03-15 00:41:19 +0200

    added Products serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,21 @@
-import json
 from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
-@api_view(["GET"])
+@api_view(['POST'])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().first()
-    data = {}
-    if model_data:
-        data = model_to_dict(model_data)
-    return Response(data)
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    print("Not Valid")
+    return None
+    # instance = Product.objects.all().first()
+    # data = {}
+    # if instance:
+    #     data = ProductSerializer(instance).data
+    # return Response(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" 
 
-get_response = requests.get(endpoint, json={"product_id": 123 }) # HTTP Request
+get_response = requests.post(endpoint, json={"name": "Hello world","stock":3 }) # HTTP Request
 # print(get_response.text) # print raw text response
 # print(get_response.status_code)
 
