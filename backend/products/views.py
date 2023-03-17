604e4f5c2b22ef0f148629ddde3540b41d00a135
costinp02 <costinpin02@gmail.com>
2023-03-17 22:32:46 +0200

    added all basic CRUD functions for Product model

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductListCreateAPIView(generics.ListCreateAPIView): #list items and can also create new items (combine create and list)
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        name = serializer.validated_data.get('name')
+        decription = serializer.validated_data.get('description') or None
+        if decription is None:
+            decription = name
+        serializer.save(description=decription)
+        # send a Django signal
+
+product_list_create_view = ProductListCreateAPIView.as_view()
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_field = 'pk' ??
+
+product_detail_view = ProductDetailAPIView.as_view()
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        # return super().perform_update(serializer)
+        instance = serializer.save()
+
+product_update_view = ProductUpdateAPIView.as_view()
+
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+product_destroy_view = ProductDestroyAPIView.as_view()
+
